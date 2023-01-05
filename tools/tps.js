74638eb733e1c599213d6fedae7bdf76c7ac8f67
Antonio Pierro <apierr>
2023-01-05 15:24:41 +0100

    feat: add tps

--- tools/tps.js
+++ tools/tps.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs').promises;
 const FNAME = 'data/blocks.csv';
+const FNAME_OUT = 'data/tps.csv';
 const frequencies = {};
 
 const main = async () => {
@@ -10,6 +11,9 @@ const main = async () => {
     const tpss = Object.values(frequencies).filter(e => !isNaN(e));
     console.log(`message:`, tpss);
     console.log(`message:`, {key: getMax(tpss)});
+    await fs.writeFile(FNAME_OUT, tpss.join("\n"), 'utf8');
+    // Aptos blockchain is supposed to handle an incredible 160,000 transactions per second (TPS) and is currently handling a significantly smaller sum of 4 TPS.
+    // https://beincrypto.com/troubled-aptos-blockchain-manages-just-4-tps/
 };
 
 const getMax = (arr) => arr.reduce((a, b) => Math. max(a, b), -Infinity);
